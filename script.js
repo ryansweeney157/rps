66fa6ac56477cbbe7544cbabf919dd7c95f2027f
RyanS <ryanweeney157@gmail.com>
2024-09-30 08:59:56 -0400

    starting javascript

---
+++ script.js
@@ -0,0 +1,3 @@
+const choices = ['rock', 'paper', 'scissors'];
+//fetches choice from html document, foreach applies elements to functions
+document.querySelectorAll('.choice').forEach(button => {})
