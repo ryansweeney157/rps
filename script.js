708554c2b497ed747ac219e0453f6e110d05bf8d
RyanS <ryanweeney157@gmail.com>
2024-09-30 09:56:48 -0400

    stating and defining javascript elements and starting functions

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const choices = ['rock', 'paper', 'scissors'];
 //fetches choice from html document, foreach applies elements to functions
-document.querySelectorAll('.choice').forEach(button => {})
+document.querySelectorAll('.choice').forEach(button => {
+    button.addEventListener('click', () => {
+        const userChoice = button.id;
+        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        const winner = getWinner (userChoice, computerChoice);
+
+        document.getElementById('user-choice').textContent = `Your choice: ${userChoice}`;
+        document.getElementById('opponent-choice').textContent = `Computer's Choice: ${computerChoice}`;
+        document.getElementById('end-result').textContent = `Winner: ${winner}`;
+    });
+
+});
